6098a31dc331ccb52f6c570b1ab1b2b89be4c9e2
amina1000 <amina1000@yandex.ru>
2021-02-25 21:51:18 +0300

    Hello world

--- src/main/java/lesson1/MainApp.java
+++ src/main/java/lesson1/MainApp.java
@@ -3,11 +3,13 @@ package lesson1;
 /**
  * homework lesson1
  *
- * @user Admin
+ * @author Admin
  * 25.02.2021
  */
 public class MainApp {
     public static void main(String[] args) {
+
+        System.out.println("Hello world");
 
     }
 }
