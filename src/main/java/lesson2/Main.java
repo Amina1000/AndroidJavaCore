1fc90d8ff7264b04a7edff746a9eb3d7041c93b7
RN <amina1000@yandex.ru>
2021-03-01 08:03:53 +0300

    Урок 2

---
+++ src/main/java/lesson2/Main.java
@@ -0,0 +1,13 @@
+package lesson2;
+
+public class Main {
+    /**
+     * homework lesson2
+     *
+     * @author Admin
+     * 01.03.2021
+     */
+    public static void main(String[] args) {
+        System.out.println("Задача 1:");
+    }
+}
