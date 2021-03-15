3fd2bd75030a3dc0e24a3f35b3d92b72757bd299
amina1000 <amina1000@yandex.ru>
2021-03-15 15:38:42 +0300

    Урок 2,3,4- урок 2, исправила ошибку

--- src/main/java/lesson2/Main.java
+++ src/main/java/lesson2/Main.java
@@ -76,13 +76,12 @@ public class Main {
     public static void fillDiagonal() {
         int[][] arr = new int[4][4];
         for (int i = 0; i < 4; i++) {
+
             for (int j = 0; j < 4; j++) {
-                if (i==j) {
-                    arr[i][j] = 1;
-                }
+                    arr[i][i] = 1;
+                    arr[i][arr.length-1-i] = 1;
                 System.out.print(arr[i][j] + " ");
-            }
-            System.out.println();
+            }System.out.println();
         }
     }
 
