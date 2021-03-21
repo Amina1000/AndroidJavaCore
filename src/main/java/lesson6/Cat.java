396ba1e7009c1fbdade34615b380cba8332891aa
amina1000 <amina1000@yandex.ru>
2021-03-21 23:28:53 +0300

    Урок 6. Задачи 1,2,3,4,5,6

---
+++ src/main/java/lesson6/Cat.java
@@ -0,0 +1,38 @@
+package lesson6;
+
+/**
+ * homework lesson6
+ *
+ * @author Amina
+ * 18.03.2021
+ */
+public class Cat {
+    String name;
+    int appetite;
+    boolean full;
+
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+    }
+
+    public boolean isFull() {
+        return full;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void eat(Bowl bowl) {
+        int food = bowl.getFood() - appetite;
+        if (food < 0) {
+            full = false;
+            bowl.putFood(Math.abs(food));
+        }else {
+            full =true;
+            bowl.decreaseFood(appetite);
+        }
+    }
+
+}
