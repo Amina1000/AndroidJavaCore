894bcc7fc4351464b50cd95e8089653761e25de0
amina1000 <amina1000@yandex.ru>
2021-04-23 02:05:30 +0300

    lesson 14. Задачи 1,2.параметрические тесты.

--- src/test/java/lesson14/ArrayMethodsTest.java
+++ src/test/java/lesson14/ArrayMethodsTest.java
@@ -4,9 +4,14 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
 import java.util.ArrayList;
 import java.util.Arrays;
-
+import java.util.List;
+import java.util.stream.Stream;
 
 
 /**
@@ -35,13 +40,32 @@ class ArrayMethodsTest {
         }
         arrayMethods = new ArrayMethods(num);
     }
-
-    @Test
-    void getElementsAfter4Test() {
-        ArrayMethods arrayMethods = new ArrayMethods(1,2,3,4,5,6,7);
-        Integer[]a = {5,6,7};
-        ArrayList<Integer> result = new ArrayList<>(Arrays.asList(a));
+    @ParameterizedTest
+    @MethodSource("dataForAddOperation")
+    public void getElementsAfter4Test(Integer[] a,ArrayList result) {
+        ArrayMethods arrayMethods = new ArrayMethods(a);
         Assertions.assertEquals(arrayMethods.getElementsAfter4(),result);
+    }
+
+    public static Stream<Arguments> dataForAddOperation() {
+        List<Arguments> out = new ArrayList<>();
+        int index=0;
+        for (int i = 0; i < 10; i++) {
+          Integer[]a = {i,2+i,i,4,5,12-i,7};
+
+            for (int j = a.length-1; j >-1; j--) {
+                if (a[j]!=4){
+                    index=j;
+                }else break;
+           }
+        Integer[]b = new Integer[a.length-index];
+            System.arraycopy(a,index,b,0,a.length-index);
+            ArrayList<Integer> result = new ArrayList<>(Arrays.asList(b));
+            out.add(Arguments.arguments(a,result));
+     }
+
+
+        return out.stream();
     }
 
     @Test
